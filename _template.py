965462ef7116a148ef9540540eab6bf7d947ced6
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2024-11-29 00:13:22 -0600

    [runner] update template

--- _template.py
+++ _template.py
@@ -3,14 +3,14 @@ def parse(fn):
         lines = [l.strip('\n') for l in f.readlines()]
     return lines
 
-def part1(output):
+def part1(output=False):
     lines = parse('data/ec_{YEAR}/{DAY}-a.txt')
     return ''
 
-def part2(output):
+def part2(output=False):
     lines = parse('data/ec_{YEAR}/{DAY}-b.txt')
     return ''
 
-def part3(output):
+def part3(output=False):
     lines = parse('data/ec_{YEAR}/{DAY}-c.txt')
     return ''
