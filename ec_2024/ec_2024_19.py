e9db98a03ff40ca5a05ccc130fdbfe7f89cf666c
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2024-11-28 23:41:02 -0600

    Improve algorithm for 2024 Day 19
    Significant speedup and also able to compute all final points to display
    part 3 image instead of only manipulating the answer-containing points.

--- ec_2024/ec_2024_19.py
+++ ec_2024/ec_2024_19.py
@@ -1,32 +1,31 @@
 from itertools import cycle
-from time import sleep
 
-import mrm.ansi_term as ansi
+from mrm.bitvector import Bitvector
 from mrm.image import print_image
 from mrm.point import adj_diag, grid_as_dict
-
 
 def parse(fn):
     with open(fn, 'r', encoding='utf8') as f:
         lines = [l.strip('\n') for l in f.readlines()]
-    return lines
 
-LEFT = [1, 2, 4, 0, 7, 3, 5, 6]
-RIGHT = [3, 0, 1, 5, 2, 6, 7, 4]
-DIRS = {'L': LEFT, 'R': RIGHT}
+    key = lines[0]
+    grid = grid_as_dict(lines[2:])
+
+    return key, grid
+
+REORDERS = {'L': [1, 2, 4, 0, 7, 3, 5, 6],
+            'R': [3, 0, 1, 5, 2, 6, 7, 4]}
 
 def rot(grid, pos, direction):
     adj = adj_diag(pos)
     iv = [grid[a] for a in adj]
 
     for i in range(8):
-        grid[adj[DIRS[direction][i]]] = iv[i]
+        grid[adj[REORDERS[direction][i]]] = iv[i]
 
 def part1(output):
-    lines = parse('data/ec_2024/19-a.txt')
+    key, grid = parse('data/ec_2024/19-a.txt')
 
-    key = lines[0]
-    grid = grid_as_dict(lines[2:])
     rot_pts = [g for g in grid if len(adj_diag(g, grid)) == 8]
 
     kv = cycle(key)
@@ -39,56 +38,50 @@ def part1(output):
     answer_pts = sorted((k, v) for k,v in grid.items() if v in '123456789')
     return ''.join([a[1] for a in answer_pts])
 
-def part2(output):
-    lines = parse('data/ec_2024/19-b.txt')
-
-    key = lines[0]
-    grid = grid_as_dict(lines[2:])
+def compute_final(grid, key, num_rounds, output):
     rot_pts = [g for g in grid if len(adj_diag(g, grid)) == 8]
+
+    rounds = Bitvector(iv=num_rounds)
+    steps = max(rounds.bits()) + 1
 
     if output:
-        ansi.clear_screen()
-
-    for n in range(100):
-        kv = cycle(key)
-        for r in rot_pts:
-            rot(grid, r, next(kv))
-
-        if output:
-            with ansi.hidden_cursor():
-                ansi.cursor_home()
-                print('Round', n + 1)
-                print_image(grid, use_char=True)
-                sleep(0.1)
-
-    answer_pts = sorted((k, v) for k,v in grid.items() if v in '123456789')
-    return ''.join([a[1] for a in answer_pts])
-
-def part3(output):
-    lines = parse('data/ec_2024/19-c.txt')
-
-    key = lines[0]
-    grid = grid_as_dict(lines[2:])
-    rot_pts = [g for g in grid if len(adj_diag(g, grid)) == 8]
+        print(f'{rounds.as_int()} rounds is {bin(rounds.as_int())} requiring 1 rotation and {steps - 1} compositions')
 
     mapping = {g: g for g in grid}
+    kv = cycle(key)
+    for r in rot_pts:
+        rot(mapping, r, next(kv))
 
-    for _ in range(100):
-        kv = cycle(key)
-        for r in rot_pts:
-            rot(mapping, r, next(kv))
+    if output:
+        print('First round built')
 
-    rev_mapping = {v: k for k, v in mapping.items()}
+    all_maps = [mapping]
+    for n in range(1, steps):
+        mapping = {k: mapping[v] for k, v in mapping.items()}
+        all_maps += [mapping]
+        if output:
+            print(f'2^{n} = {2**n} composed')
 
-    key_pts   = [k for k, v in grid.items() if v in '123456789']
-    key_vals  = [v for k, v in grid.items() if v in '123456789']
+    if output:
+        print('Applying bits', rounds.bits())
 
-    its = 1048576000 // 100
-    for n in range(its):
-        new_key_pts = [rev_mapping[k] for k in key_pts]
-        key_pts = new_key_pts
-        if output and n % 100000 == 0:
-            print(f'Round {n:>12d}, {n / its * 100:6.2f}%')
+    mapping = {g: g for g in grid}
+    for b in rounds.bits():
+        mapping = {k: all_maps[b][v] for k, v in mapping.items()}
+        if output:
+            print(f'2^{b} = {2**b} applied')
 
-    answer_pts = sorted((k, v) for k, v in zip(key_pts, key_vals))
+    if output:
+        disp_grid = {k: grid[v] for k, v in mapping.items()}
+        print_image(disp_grid, use_char=True)
+
+    answer_pts = sorted((k, grid[v]) for k, v in mapping.items() if grid[v] in '123456789')
     return ''.join([a[1] for a in answer_pts])
+
+def part2(output):
+    key, grid = parse('data/ec_2024/19-b.txt')
+    return compute_final(grid, key, 100, output)
+
+def part3(output):
+    key, grid = parse('data/ec_2024/19-c.txt')
+    return compute_final(grid, key, 1048576000, output)
