64880bb1bca02baa0d25502d4b9826ff62f38214
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2024-11-28 23:34:48 -0600

    [util] docs/typing/updates for Bitvector

--- mrm/bitvector.py
+++ mrm/bitvector.py
@@ -1,22 +1,48 @@
+"""Module for a bitvector object with easily manipulated bits"""
+
+__all__ = ['Bitvector']
+
 from functools import total_ordering
-from math import ceil
+from math import ceil, log2
 
 @total_ordering
 class Bitvector:
-    def __init__(self, max_bit, iv=0):
+    """Bitvectors provide convenient access and manipulation of individual binary bits"""
+
+    def __init__(self, *, max_bit: None|int=None, iv: int=0):
+        """Creates a new Bitvector with initial value iv and with largest bit at least max_bit.
+        If max_bit is None or not specified, it will be set to the closest multiple of 8
+        capable of holding the iv.  If it is specified, it will also be set to the next highest
+        multiple of 8 bits.
+        """
+
+        if iv < 0:
+            raise ValueError(f'{iv=} must be >= 0')
+
+        if iv == 0:
+            need_bits = 1
+        else:
+            need_bits = ceil(log2(iv))
+
+        if max_bit is None:
+            max_bit = need_bits
+
+        if max_bit < need_bits:
+            raise ValueError(f'{iv=} needs {ceil(log2(iv))} bits, which is less than {max_bit=}')
+
         self.max_bit = ceil((max_bit + 1) / 8) * 8 - 1
         self.data = bytearray((self.max_bit + 1) // 8)
-        if iv != 0:
-            for i, b in enumerate(reversed(bin(iv)[2:])):
-                if b == '1':
-                    self.__setitem__(i, b)
+        for i, b in enumerate(reversed(bin(iv)[2:])):
+            self[i] = b == '1'
 
-    def __getitem__(self, bit):
+    def __getitem__(self, bit: int) -> bool:
+        """Returns the current state of the specified bit index"""
         if bit < 0 or bit > self.max_bit:
             raise IndexError(f'bit index out of range 0 to {self.max_bit}')
-        return self.data[bit // 8] >> (bit % 8) & 0x01
+        return self.data[bit // 8] >> (bit % 8) & 0x01 == 1
 
-    def __setitem__(self, bit, val):
+    def __setitem__(self, bit: int, val: bool):
+        """Sets the state of the specified bit index to val"""
         if bit < 0 or bit > self.max_bit:
             raise IndexError(f'bit index out of range 0 to {self.max_bit}')
         if val:
@@ -24,39 +50,57 @@ class Bitvector:
         else:
             self.data[bit // 8] &= ~(1 << (bit % 8))
 
-    def as_int(self):
+    def as_int(self) -> int:
+        """Returns the set bits as an unsigned int"""
         return int.from_bytes(self.data, byteorder='little')
 
-    def bits(self):
-        return [x for x in range(self.max_bit) if self.__getitem__(x)]
+    def bits(self) -> list[int]:
+        """Returns a list of currently set bit numbers"""
+        return [x for x in range(self.max_bit + 1) if self[x]]
 
-    def without(self, bit):
-        self.__setitem__(bit, 0)
+    def without(self, bit: int) -> 'Bitvector':
+        """Chainable function to clear a specific bit
+        (returns the Bitvector being operated on)
+        """
+        self[bit] = False
         return self
 
-    def with_(self, bit):
-        self.__setitem__(bit, 1)
+    def with_(self, bit: int) -> 'Bitvector':
+        """Chainable item to set a specific bit
+        (returns the Bitvector being operated on)
+        """
+        self[bit] = True
         return self
 
     def __repr__(self):
         return 'bitvect<' + bin(self.as_int()) + '>'
 
-    def __lt__(self, oth):
-        if not isinstance(oth, Bitvector):
+    def __lt__(self, other) -> bool:
+        """Returns a < b for two Bitvectors,
+        which are compared based on their as_int() values.
+        """
+        if not isinstance(other, Bitvector):
             return NotImplemented
-        return self.as_int() < oth.as_int()
+        return self.as_int() < other.as_int()
 
-    def __eq__(self, oth):
-        if not isinstance(oth, Bitvector):
+    def __eq__(self, other) -> bool:
+        """Returns a == b for two Bitvectors,
+        which are compared based on their as_int() values.
+        """
+        if not isinstance(other, Bitvector):
             return NotImplemented
-        return self.as_int() == oth.as_int()
+        return self.as_int() == other.as_int()
 
-    def __hash__(self):
+    def __hash__(self) -> int:
+        """Returns hash of Bitvector, which is its as_int() value."""
         return self.as_int()
 
     @staticmethod
-    def from_bits(max_bit, bit_list):
-        v = Bitvector(max_bit)
+    def from_bits(bit_list: list[int], max_bit: None|int=None) -> 'Bitvector':
+        """Creates a Bitvector with all bits in bit_list preset.
+        max_bit may be specified or it will be calculated automatically.
+        """
+        v = Bitvector(max_bit=max_bit)
         for b in bit_list:
             v[b] = True
         return v
