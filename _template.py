71400a80a88896608a71719e8fc8a799b07e9fe2
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2025-08-29 02:47:07 -0500

    Update template

--- _template.py
+++ _template.py
@@ -1,3 +1,28 @@
+from collections import Counter, defaultdict, deque, namedtuple
+from functools import cache, cmp_to_key, partial, reduce
+from itertools import combinations, cycle, groupby, pairwise, permutations, product
+import math
+import operator
+import random
+import re
+
+import mrm.ansi_term as ansi
+from mrm.bitvector import Bitvector
+from mrm.cache import Keycache
+import mrm.cpoint as cpt
+from mrm.crt import all_coprime, coprime, crt
+from mrm.dijkstra import Dictlike, dijkstra
+from mrm.graph import bfs_dist, bfs_min_paths, connected_component, prim_mst
+import mrm.image as img
+from mrm.iter import batched, flatten_lists, sliding_window
+import mrm.llist as llist
+from mrm.parse import all_nums, ensure_equal_length
+import mrm.point as pt
+from mrm.search import fn_binary_search
+from mrm.text import let2num, num2let
+from mrm.tsp import held_karp, held_karp_dist
+from mrm.util import big_pi, Funkydict, md5sum
+
 def parse(fn):
     with open(fn, 'r', encoding='utf8') as f:
         lines = [l.strip('\n') for l in f.readlines()]
