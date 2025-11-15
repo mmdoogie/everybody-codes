b0c15928209a24395b566dc7faa162e6f8658b98
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2025-11-15 03:40:12 -0600

    [util] Add max heap version of Prioset

--- mrm/prioset.py
+++ mrm/prioset.py
@@ -8,9 +8,14 @@ class Prioset:
     """Priority Set
     Provides a priority queue implemented with sets for faster insertion & membership checking
     """
-    def __init__(self):
-        self._sets = {0: set()}
-        self._prios = [0]
+    def __init__(self, max_heap=False):
+        """Create a new empty Priority Set
+
+        max_heap -- if True, uses a max heap, where pop instead returns an item from the maximal priority level
+        """
+        self._sets = {}
+        self._prios = []
+        self._max_heap = max_heap
 
     def add(self, item, prio):
         """Add a new item with associated priority level
@@ -18,6 +23,8 @@ class Prioset:
         item -- item to add
         prio -- priority of item to add
         """
+        if self._max_heap:
+            prio = -prio
         if prio in self._sets:
             self._sets[prio].add(item)
             return
@@ -36,10 +43,14 @@ class Prioset:
         if not self._sets[prio]:
             heapq.heappop(self._prios)
             del self._sets[prio]
+        if self._max_heap:
+            prio = -prio
         return item, prio
 
     def prios(self):
         """Get the list of priority levels present"""
+        if self._max_heap:
+            return sorted((-s for s in self._prios), reverse=True)
         return sorted(self._prios)
 
     def __bool__(self):
@@ -59,6 +70,11 @@ class Prioset:
 
     def __getitem__(self, key):
         """Returns the set of items for a given priority level"""
+        if self._max_heap:
+            key = -key
         if key in self._sets:
             return self._sets[key]
-        raise ValueError(f'Priority level {key} not present')
+        raise IndexError(f'Priority level {key} not present')
+
+    def __iter__(self):
+        return iter(self.prios())
