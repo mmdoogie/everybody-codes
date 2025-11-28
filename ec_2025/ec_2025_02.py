6fda1cf32c490a4ff73cd34cf2f03a18dc33bd73
mmdoogie <24367033+mmdoogie@users.noreply.github.com>
2025-11-28 16:38:52 -0600

    Update Day 2 for faster math

--- ec_2025/ec_2025_02.py
+++ ec_2025/ec_2025_02.py
@@ -1,34 +1,19 @@
 import mrm.ansi_term as ansi
-import mrm.cpoint as cpt
 from mrm.parse import all_nums
 
 def parse(fn):
     with open(fn, 'r', encoding='utf8') as f:
         lines = [l.strip('\n') for l in f.readlines()]
-    return cpt.from_xy(*all_nums(lines[0]))
-
-def trunc_div(a, b):
-    return cpt.from_xy(int(a.real / b.real), int(a.imag / b.imag))
-
-def cycle(r, a, divisor):
-    r = r * r
-    r = trunc_div(r, divisor)
-    r = r + a
-    return r
-
-def check_lim(r):
-    return -1000000 <= r.real <= 1000000 and -1000000 <= r.imag <= 1000000
+    return tuple(all_nums(lines[0]))
 
 def part1(output=False):
     a = parse('data/ec_2025/02-a.txt')
 
-    r = cpt.from_xy(0, 0)
-    divisor = cpt.from_xy(10, 10)
+    rx, ry = 0, 0
     for _ in range(3):
-        r = cycle(r, a, divisor)
-    r = cpt.as_xy(r, int)
+        rx, ry = int((rx * rx - ry * ry) / 10) + a[0], int((rx * ry + rx * ry) / 10) + a[1]
 
-    return f'[{r[0]},{r[1]}]'
+    return f'[{rx},{ry}]'
 
 def part2(output=False):
     a = parse('data/ec_2025/02-b.txt')
@@ -36,18 +21,18 @@ def part2(output=False):
     palette = [ansi.red, ansi.magenta, ansi.blue, ansi.cyan, ansi.green, ansi.yellow]
 
     engrave = 0
-    divisor = cpt.from_xy(100000, 100000)
     for y in range(0, 1001, 10):
         for x in range(0, 1001, 10):
-            r = cpt.from_xy(0, 0)
-            p = cpt.from_xy(x, y) + a
+            rx, ry = 0, 0
+            px, py = x + a[0], y + a[1]
+            ok = True
             for i in range(100):
-                r = cycle(r, p, divisor)
-                ok = check_lim(r)
+                rx, ry = int((rx * rx - ry * ry) / 100000) + px, int((rx * ry + rx * ry) / 100000) + py
+                ok = -1000000 <= rx <= 1000000 and -1000000 <= ry <= 1000000
                 if not ok:
                     break
             if output:
-                print(palette[i//20]('**') if not ok else '  ', end='')
+                print(palette[(i-21)//14]('**') if not ok else '  ', end='')
             if ok:
                 engrave += 1
         if output:
@@ -59,14 +44,14 @@ def part3(output=False):
     a = parse('data/ec_2025/02-c.txt')
 
     engrave = 0
-    divisor = cpt.from_xy(100000, 100000)
-    for y in range(0, 1001, 1):
-        for x in range(0, 1001, 1):
-            r = cpt.from_xy(0, 0)
-            p = cpt.from_xy(x, y) + a
-            for _ in range(100):
-                r = cycle(r, p, divisor)
-                ok = check_lim(r)
+    for y in range(0, 1001):
+        for x in range(0, 1001):
+            rx, ry = 0, 0
+            px, py = x + a[0], y + a[1]
+            ok = True
+            for i in range(100):
+                rx, ry = int((rx * rx - ry * ry) / 100000) + px, int((rx * ry + rx * ry) / 100000) + py
+                ok = -1000000 <= rx <= 1000000 and -1000000 <= ry <= 1000000
                 if not ok:
                     break
             if ok:
